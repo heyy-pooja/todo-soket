75803156d2c25816f1640916b78456a751cfcbc9
pooja <pooja@gamil.com>
2024-08-26 14:17:24 +0530

    controller code done

--- controller/todo.controller.js
+++ controller/todo.controller.js
@@ -1,14 +1,19 @@
 const asyncHandler = require("express-async-handler")
+const Todo = require("../model/Todo")
 
 exports.createTodos = asyncHandler(async (req, res) => {
-    res.json({ message: "create Todo Success" })
+    const result = await Todo.findOne()
+    res.json({ message: "create Todo Success", result })
 })
 exports.readTodos = asyncHandler(async (req, res) => {
+    await Todo.create(req.body)
     res.json({ message: "read Todo Success" })
 })
 exports.updateTodos = asyncHandler(async (req, res) => {
+    await Todo.findByIdAndUpdate(req.body, req.params.id)
     res.json({ message: "update Todo Success" })
 })
 exports.deleteTodos = asyncHandler(async (req, res) => {
+    await Todo.findByIdAndDelete(req.params.id)
     res.json({ message: "delete Todo Success" })
 })
