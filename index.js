baff4af47ffdcec623cc94f974bcc8a141b78e09
pooja <pooja@gamil.com>
2024-08-27 10:40:39 +0530

    index file change

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ app.use(cors({ origin: true, credentials: true }))
 
 
 // step 2 routes
-app.use("./api/notes", require("./routes/todo.routes"))
+app.use("/api/notes", require("./routes/todo.routes"))
 
 // step 3 404 route
 app.use("*", (req, res) => {
-    res.status.json({ message: "resource not found" })
+    res.status(404).json({ message: "resource not found" })
 })
 // step 4 error handler
 app.use((err, req, res, next) => {
